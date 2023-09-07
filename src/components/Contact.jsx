7381753a7d3d4cbb6dfe457cc5c19b6267bc5085
ARojasPolanco <advbrrop23@gmail.com>
2023-09-07 02:02:05 -0300

    code cleanup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = ({ setShowModal }) => {
                 <h2>Contacto</h2>
                 <div className="h-[2px] w-[150px] bg-white/70"></div>
             </article>
-            <form onSubmit={sendEmail} className='flex flex-col p-2 gap-2 items-center bg-light-gray/20 w-[280px] border-[1px] border-light-gray rounded-md'>
+            <form onSubmit={sendEmail} className='flex flex-col p-2 gap-2 bg-light-gray/20 w-[300px] border-[1px] border-light-gray rounded-md min-[645px]:w-[630px]'>
                 <label className='font-montserrat text-white'>Nombre</label>
                 <input autoComplete='off' type="text" name="user_name" className='bg-light-gray text-white outline-none' />
                 <label className='font-montserrat text-white'>Email</label>
@@ -31,6 +31,17 @@ const Contact = ({ setShowModal }) => {
                 <textarea name="message" rows={10} cols={25} className='resize-none bg-light-gray text-white outline-none' />
                 <input type="submit" value="Enviar" className='bg-light-gray w-[150px] rounded-md text-white font-montserrat cursor-pointer p-1 border-[1px] outline-none' />
             </form>
+            <article className='flex items-center justify-center gap-8 text-[40px] text-white'>
+                <div className='hover:scale-110 cursor-pointer'>
+                    <i className='bx bxl-whatsapp'></i>
+                </div>
+                <div className='hover:scale-110 cursor-pointer'>
+                    <i className='bx bxl-github'></i>
+                </div>
+                <div className='hover:scale-110 cursor-pointer'>
+                    <i className='bx bxl-linkedin' ></i>
+                </div>
+            </article>
         </section>
     )
 }
